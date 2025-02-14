ee97f4a5bf1a36c2a82c00bee3ab54a2f47f41fe
HARIPRAKASSH. P <125394864+hariprakassh1112@users.noreply.github.com>
2025-02-14 19:39:32 +0530

    Delete application/auth directory

--- application/auth/forgot_password_form/forgot_password_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'forgot_password_bloc.dart';
-
-@freezed
-abstract class ForgotPasswordEvent with _$ForgotPasswordEvent {
-  const factory ForgotPasswordEvent.emailChanged(String emailStr) =
-      EmailChanged;
-  const factory ForgotPasswordEvent.sendPasswordResetEmailPressed() =
-      SendPasswordResetEmailPressed;
-}
